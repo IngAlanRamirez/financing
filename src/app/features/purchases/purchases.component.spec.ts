df28ed93c924e772cfdc16eb83a46b70696c3259
Alan Christian Ramírez Rodríguez <ingenieroacramirez@gmail.com>
2025-03-24 21:16:29 -0600

    Eliminar archivo CSS de compras y ajustar estilos en SCSS

--- src/app/features/purchases/purchases.component.spec.ts
+++ src/app/features/purchases/purchases.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import PurchasesComponent from './purchases.component';
 
-import { PurchasesComponent } from './purchases.component';
 
 describe('PurchasesComponent', () => {
   let component: PurchasesComponent;
